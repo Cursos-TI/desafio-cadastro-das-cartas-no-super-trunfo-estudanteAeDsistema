73dae6dec6f6fd89861a10933de494ee4be2a900
estudanteAeDsistema <viviovalenca20@gmail.com>
2025-04-07 19:30:08 +0000

    desafio 03

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,18 +2,36 @@
 
 int main(){
 
-    char estado
+    char estado1
     char codigo
     char cidade
     int população
     float área
-    float pib
+    float PIB
     int turisticos
 
     printf("digite seu estado: %c\n, estado");
     scanf("%c", &estado);
-    printf("codigo: %c\n, codigo");
 
+    printf("codigo: %c\n, codigo");
+    scanf("%c", &codigo);
+
+    printf("digite sua cidade: %c\n, cidade");
+    scanf("%c", &cidade);
+
+    printf("digite a população: %d\n, população");
+    scanf("%d", &população);
+
+    printf("digite a área: %f\n, área");
+    scanf("%f", &área);
+
+    printf("qual o PIB: %f\n, pib");
+    scanf("%f", &PIB);
+
+    printf("pontos turisticos: %d\n, turisticos");
+    scanf("%d", &turisticos);
+
+ 
 
 
 
