690cd8c25a9e9cd4fe6c68392f573c671d8fff5d
estudanteAeDsistema <viviovalenca20@gmail.com>
2025-04-07 18:32:12 +0000

    desafio Cartas Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main() {
+int main(){
 
     char estado = 'piauí';
     char codigo = 'A01';
     char cidade = "teresina";
     int população = 3.37;
-    float área = 251.529
-    float PIB = 6.12
+    float área = 251.529;
+    float pib = 3.12;
     int turisticos = 20;
 
 
