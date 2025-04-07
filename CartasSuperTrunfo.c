faa3d98af259fd5c700de347bd0243a04e5ca640
estudanteAeDsistema <viviovalenca20@gmail.com>
2025-04-07 18:06:45 +0000

    Desafio Cartas Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    char estado = piauí
-    char[20] código da carta = A01
-    char[20] nome da cidade = teresina
-    int população = 3.375.646
-    float área em km² = 251.529
-    float PIB = R$ 64.000.000
-    int número de pontos turisticos = 20
+
+    char estado = 'piauí';
+    char codigo = 'A01';
+    char cidade = "teresina";
+    int população = 3.37;
+    float área = 251.529
+    float PIB = 6.12
+    int turisticos = 20;
 
 
-
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     return 0;
 }
